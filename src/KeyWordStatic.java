a61e0446d06190f2216a5eba96bacf316ed7dd01
unknown <lbvbxlbvbx@gmail.com>
2020-02-03 17:11:40 +0200

    commit 03.02.20

--- src/KeyWordStatic.java
+++ src/KeyWordStatic.java
@@ -9,20 +9,38 @@ public class KeyWordStatic {
         System.out.println(man2.getAge());
         System.out.println(man2.getName());
         System.out.println();
+        //пример измениния статического поля класса
+        People.description="Man";
+        People man3=new People();
+        System.out.println(man3.getDescription());
+        System.out.println(man1.getDescription());
+        System.out.println(man2.getDescription());
+        System.out.println(People.coube(2));
+        System.out.println(People.getCountPeople());
     }
 }
 class People{
     private String name;
     private int age;
+    private static int countPeople=0;
+    // ключевое слово static означает что медот или поле относится не к одному єкземляру класса(сосданного обььекта)
+    // а ко всему классу. Таким образом к параметрам и методам обозначенными ключевим словом static можно обратиться не создавая єкземпляр класса
+    // и данное изменение будет относиться ко всем обьектам данного класса
+    // Эта ебала для того, что при создании экземпляра класса создаются новые переменные для каждого отдельного обьекта, но если какой то параметр имеет описание static
+    // то такая переменна создается один раз и все обьекты ссылаються на одну и ту же ячеку памяти, где находится значение данного поля.
+    public static String description;
+    public static int x;
 
     public People(){
         this.name="Dafult name";
         this.age=0;
+        countPeople++;
     }
 
     public People(String name, int age){
         setName(name);
         setAge(age);
+        countPeople++;
     }
 
     public void setName(String name) {
@@ -40,4 +58,14 @@ class People{
     public int getAge() {
         return age;
     }
+
+    public String getDescription(){ return description; }
+
+    public static int coube(int x){// Статический метод, он работает вне зависимости от создания обьекта класса
+        int result;                // можно вызвать People.coube(2) и будет збс
+        result=x*x;
+        return result;
+    }
+    public static int getCountPeople(){return countPeople;}
+
 }
